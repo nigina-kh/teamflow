2de85d9450996af71b6333dba0de39b132a0bb54
nigina_khasanova <nigina_khasanova@student.itpu.uz>
2026-07-22 22:33:12 +0500

    feat: setup nestjs modules and prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ProjectsModule } from './projects/projects.module';
+import { BoardsModule } from './boards/boards.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
+import { LabelsModule } from './labels/labels.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, UsersModule, WorkspacesModule, ProjectsModule, BoardsModule, TasksModule, CommentsModule, LabelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
