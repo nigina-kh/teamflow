65af698be5562e25470900aacc29b5de99e14e05
nigina_khasanova <nigina_khasanova@student.itpu.uz>
2026-07-24 21:45:23 +0500

    feat: add recent tasks widget

--- frontend/src/app/shared/components/recent-tasks/recent-tasks.ts
+++ frontend/src/app/shared/components/recent-tasks/recent-tasks.ts
@@ -2,8 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recent-tasks',
+  standalone: true,
   imports: [],
   templateUrl: './recent-tasks.html',
-  styleUrl: './recent-tasks.scss',
+  styleUrl: './recent-tasks.scss'
 })
-export class RecentTasks {}
+export class RecentTasks {
+
+  tasks = [
+    {
+      title: 'Design Dashboard UI',
+      priority: 'High',
+      due: 'Today'
+    },
+    {
+      title: 'Fix Login Validation',
+      priority: 'Medium',
+      due: 'Tomorrow'
+    },
+    {
+      title: 'Update Documentation',
+      priority: 'Low',
+      due: 'Jul 28'
+    }
+  ];
+
+}
